e22896489dcf726ceefd07ab0c9925f8877be955
alvarodawserver <alvarovidalcanas21@gmail.com>
2025-11-10 21:27:08 +0100

    Mas cositas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/hola',function(Request $request) {
+    $nombre = $request ->query('nombre');
+    return "Hola,$nombre";
 });
