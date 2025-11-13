cb62499cc770f00c60abf88a895f10f4d9a80e05
alvarodawserver <alvarovidalcanas21@gmail.com>
2025-11-13 19:34:30 +0100

    Primeros avances con laravel

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,47 @@
 <?php
 
+use App\Models\Cliente;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::get('/hola',function(Request $request) {
-    $nombre = $request ->query('nombre');
-    return "Hola,$nombre";
+Route::get('/clientes',function (){
+    return view('clientes.index',[
+        'clientes' => Cliente::all(),
+    ]);
+
 });
+
+Route::get('/clientes/create',function (){
+    return view("clientes.create");
+    // Cliente::create([
+    //  "dni" => '11113',
+    //  "nombre" => 'Pepe',
+    //  "apellidos" => 'Perez',
+    //  "direccion" => 'Calle Ancha',
+    //  "codpostal" => 11540,
+    //  "telefono" => '123456789',
+    // ]);
+
+});
+
+
+Route::delete('/clientes/borrar/{cliente}', function (Cliente $cliente) {
+    $cliente->delete();
+    return redirect('/clientes');
+});
+
+//Todas estas formas de hacerlo, son "técnicas", podemos usar una fachada, un helper,etc...
+/*Route::get('/hola',function (){
+    $nombre = request() -> query('nombre'); //Esto es un helper
+    return "Hola,$nombre";
+});*/
+/*Route::get('/hola',function() { //Usamos una fachada(facade), son clases que solo tienen métodos estáticos
+    $nombre = Request::query('nombre');
+    return "Hola,$nombre";
+});*/
+
