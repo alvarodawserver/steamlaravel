5f651098fb0e804ab211f3f99278e9e30c54db12
alvarodawserver <alvarovidalcanas21@gmail.com>
2025-11-14 20:41:05 +0100

    Cambios para que mantenga los datos anteriores en el form

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -13,22 +13,22 @@
     <form action="/clientes/create" method="post">
         @csrf
         <label for="dni">DNI:*</label>
-        <input type="text" id="dni" name="dni" value=""><br>
+        <input type="text" id="dni" name="dni" value="{{ old('dni') }}"><br>
 
         <label for="nombre">Nombre:*</label>
-        <input type="text" id="nombre" name="nombre" value=""><br>
+        <input type="text" id="nombre" name="nombre" value="{{ old('nombre')  }}"><br>
 
         <label for="apellidos">Apellidos:</label>
-        <input type="text" id="apellidos" name="apellidos" value=""><br>
+        <input type="text" id="apellidos" name="apellidos" value="{{ old('apellidos')}}"><br>
 
         <label for="direccion">Dirección:</label>
-        <input type="text" id="direccion" name="direccion" value=""><br>
+        <input type="text" id="direccion" name="direccion" value="{{ old('direccion')}}"><br>
 
         <label for="codpostal">Código postal: </label>
-        <input type="text" id="codpostal" name="codpostal" value=""><br>
+        <input type="text" id="codpostal" name="codpostal" value="{{ old('codpostal')}}"><br>
 
         <label for="telefono">Teléfono: </label>
-        <input type="text" id="telefono" name="telefono" value=""><br>
+        <input type="text" id="telefono" name="telefono" value="{{ old('telefono')}}"><br>
 
         <button type="submit">Insertar</button>
     </form>
