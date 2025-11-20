e95d9ec45719750eb9fe6543467b285405a6a7b0
alvarodawserver <alvarovidalcanas21@gmail.com>
2025-11-20 13:05:18 +0100

    Update Videojuego.php

--- app/Models/Videojuego.php
+++ app/Models/Videojuego.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Videojuego extends Model
 {
+    protected $fillable = ['titulo','precio','lanzamiento','desarrolladora_id'];
     public function desarrolladora(){
         return $this->hasOne(Desarrolladora::class);
     }
